ec73361b745bb9d5682672d02852776012c6943b
arifulwasim <arifulwasim@gmail.com>
2025-01-22 11:39:51 +0600

    initail setup done

---
+++ js/app.js
@@ -0,0 +1,17 @@
+// fontawesome
+
+<script src="https://kit.fontawesome.com/ca7653cf02.js" crossorigin="anonymous"></script>;
+
+<script src="https://cdn.tailwindcss.com"></script>;
+
+<script>
+    tailwind.config = {
+        theme: {
+        extend: {
+        colors: {
+        clifford: '#da373d',
+      }
+    }
+  }
+}
+</script>;
